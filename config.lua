223f7f1d930560b8cea3e5b050f7b2bea6ec81c6
BayloraiEX <117157428+BayloraiEX@users.noreply.github.com>
2024-06-17 23:49:07 -0700

    Add files via upload

--- config.lua
+++ config.lua
@@ -1,5 +1,36 @@
 Config = {}
+----- | PED STUFF | -----
+Config.hornysBurgerGaragePed = {
+    {
+        hornysGaragePedModel = 'mp_m_waremech_01',
+        hornysGaragePedLocation = vec4(1247.36, -350.52, 68.09, 347.06),
+        hornysGarageRenderDistance = 20,
+    }
+}
 
------- | PED STUFF | ------
-config.hornysBurgerGaragePedModel = 'mp_m_waremech_01'
-config.hornysBurgerGaragePedLocation = vec4(500.04, 423.02, 465.42, 65)
+----- | VEHICLE STUFF | -----
+Config.VehicleSpawn = vector4(1249.99, -341.62, 69.08, 79.45)
+Config.Vehicle = 'nspeedo' -- Custom Vehicle ( Check 'hornysburger-van' File)
+
+----- | SHOP ITEMS | -----
+Config.Items = {
+    { name = 'hb_bun', amount = 50, price = 1 },
+    { name = 'hb_bread', amount = 50, price = 1 },
+    { name = 'hb_wraps', amount = 50, price = 1 },
+    { name = 'hb_cheddar', amount = 50, price = 1 },
+    { name = 'hb_gratedcheese', amount = 50, price = 1 },
+    { name = 'hb_lettuce', amount = 50, price = 1 },
+    { name = 'hb_pickle', amount = 50, price = 1 },
+    { name = 'hb_potatoes', amount = 50, price = 1 },
+    { name = 'hb_tomato', amount = 50, price = 1 },
+    { name = 'hb_cherrys', amount = 50, price = 1 },
+    { name = 'hb_eggs', amount = 50, price = 1 },
+    { name = 'hb_bacon', amount = 50, price = 1 },
+    { name = 'hb_chicken_strips_raw', amount = 50, price = 1 },
+    { name = 'hb_chicken', amount = 50, price = 1 },
+    { name = 'hb_patty_raw', amount = 50, price = 1 },
+    { name = 'hb_sausage', amount = 50, price = 1 },
+    { name = 'hb_icecream_mix', amount = 50, price = 1 },
+    { name = 'hb_carbonated_water', amount = 50, price = 1 },
+    { name = 'hb_sugar_cubes', amount = 50, price = 1 }
+}
